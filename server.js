ef8f682be3ea8aa1a2de55ccd37182cce942aca7
Ken Ilo <keneilo91@yahoo.com>
2021-11-08 15:05:20 -0600

    homepage

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
+// handlebar
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebar
+const hbs = exphbs.create({});
+
+// handlebar
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// added to connect path to style.css
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 // force: true performs similarly to DROP TABLE IF EXISTS in SQL
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
