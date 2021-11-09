daeb7a328488d56e61e244da7a3f276f6e773d88
Ken Ilo <keneilo91@yahoo.com>
2021-11-09 11:22:06 -0600

    login and logout

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+// session used to store reques such as user log in
+const session = require('express-session');
 const routes = require('./controllers/');
 // handlebar
 const exphbs = require('express-handlebars');
@@ -7,6 +9,20 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // handlebar
 const hbs = exphbs.create({});
@@ -19,6 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // added to connect path to style.css
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 // turn on routes
 app.use(routes);
